11b21ca5bf63b20dcf23f1ec588efa2fd16cdd4e
nokuphiwam <49691242+nokuphiwam@users.noreply.github.com>
2022-08-31 15:08:29 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -7,6 +7,8 @@
         $conn = new PDO("mysql:host=$servername;dbname=lms_attendance_tracker", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->query("CREATE DATABASE IF NOT EXISTS lms_attendance_tracker");
+        $conn->query("use lms_attendance_tracker");
         //echo "Connected successfully";
         } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
